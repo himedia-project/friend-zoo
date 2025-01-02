765236815cee36a271c3f79c1366fe8d927537a1
seonggon do <mooh2jj@naver.com>
2025-01-02 10:07:19 +0900

    상품, 콘텐츠 이미지 불러오기 api 구현

--- src/main/java/com/friendzoo/api/domain/content/controller/AdminContentController.java
+++ src/main/java/com/friendzoo/api/domain/content/controller/AdminContentController.java
@@ -5,8 +5,10 @@ import com.friendzoo.api.domain.content.dto.ContentDTO;
 import com.friendzoo.api.domain.content.service.AdminContentService;
 import com.friendzoo.api.dto.PageRequestDTO;
 import com.friendzoo.api.dto.PageResponseDTO;
+import com.friendzoo.api.util.file.CustomFileUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AdminContentController {
 
+    private final CustomFileUtil fileUtil;
     private final AdminContentService contentService;
 
     @GetMapping("/list")
@@ -58,5 +61,12 @@ public class AdminContentController {
         log.info("remove..............." + id);
         contentService.remove(id);
         return ResponseEntity.ok("success");
+    }
+
+
+    // 이미지 불러오기
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName) {
+        return fileUtil.getFile(fileName);
     }
 }
