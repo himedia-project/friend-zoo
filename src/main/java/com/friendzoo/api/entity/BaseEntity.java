04a51b36c0d87221bb8b6f5de40e8ba8f345af70
seonggon do <mooh2jj@naver.com>
2025-01-09 18:20:31 +0900

    타임존 서울로 변경 초기화 수정

--- src/main/java/com/friendzoo/api/entity/BaseEntity.java
+++ src/main/java/com/friendzoo/api/entity/BaseEntity.java
@@ -11,6 +11,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,9 +24,9 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false, columnDefinition = "TIMESTAMP")
-    private LocalDateTime createdAt;
+    private Timestamp createdAt;
 
     @LastModifiedDate
     @Column(name = "modified_at", columnDefinition = "TIMESTAMP")
-    private LocalDateTime modifiedAt;
+    private Timestamp modifiedAt;
 }
